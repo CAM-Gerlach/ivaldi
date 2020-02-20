9801f42c3c0c2f38535970fe0f058e462b5e3a7b
C.A.M. Gerlach <CAM.Gerlach@Gerlach.CAM>
2020-02-19 23:41:48 -0600

    Refactor functionality into an Analog mixin and abstract ADC

--- src/ivaldi/devices/adafruit.py
+++ src/ivaldi/devices/adafruit.py
@@ -4,6 +4,7 @@ Adafruit I2C weather sensor devices.
 
 # Third party imports
 import adafruit_ads1x15.ads1115
+import adafruit_ads1x15.analog_in
 import adafruit_bmp280
 import adafruit_sht31d
 import board
@@ -13,9 +14,20 @@ import busio
 class AdafruitADS1115(adafruit_ads1x15.ads1115.ADS1115):
     """A thin shim on top of the Adafruit ADS11115 ADC class."""
 
-    def __init__(self):
+    def __init__(self, channel=0):
         i2c = busio.I2C(board.SCL, board.SDA)
         super().__init__(i2c)
+        self._adc = adafruit_ads1x15.analog_in.AnalogIn(self, channel)
+
+    @property
+    def voltage(self):
+        """The voltage reported by the ADC, in volts."""
+        return self._adc.voltage
+
+    @property
+    def raw_value(self):
+        """The raw value reported by the ADC, as a 16-bit integer."""
+        return self._adc.value
 
 
 class AdafruitBMP280(adafruit_bmp280.Adafruit_BMP280_I2C):
--- src/ivaldi/devices/analog.py
+++ src/ivaldi/devices/analog.py
@@ -2,19 +2,41 @@
 Analog devices that need to be ADC converted.
 """
 
-# Third party imports
-import adafruit_ads1x15.ads1115
-import adafruit_ads1x15.analog_in
-
 # Local imports
 import ivaldi.devices.adafruit
 
-
+# Wind direction constants
 WIND_DEFAULT_OFFSET = 4.66
 WIND_DEFAULT_SCALE = 0.013113
 
 
-class AnemometerDirection():
+class AnalogMeasurementMixin():
+    """
+    Class for an analog measurement sensor device.
+
+    Parameters
+    ----------
+    scale : numeric, optional
+        The value to scale the output by. The default is 1.
+    offset : numeric, optional
+        The value to offset the output by. The default is 0.
+
+    """
+
+    def __init__(self, scale=1, offset=0, **adc_args):
+        """See class docstring for full details."""
+        super().__init__(**adc_args)
+        self.scale = scale
+        self.offset = offset
+
+    @property
+    def value(self):
+        """The value of the quantity measured, in physical units."""
+        return self.raw_value * self.scale + self.offset
+
+
+class AnemometerDirection(AnalogMeasurementMixin,
+                          ivaldi.devices.adafruit.AdafruitADS1115):
     """
     Class for an analog wind direction sensor.
 
@@ -25,31 +47,13 @@ class AnemometerDirection():
     offset : numeric, optional
         The value to offset the output by. The default is 4.66.
 
-    Returns
-    -------
-    None.
-
     """
 
-    def __init__(self, scale=WIND_DEFAULT_SCALE, offset=WIND_DEFAULT_OFFSET):
+    def __init__(
+            self,
+            scale=WIND_DEFAULT_SCALE,
+            offset=WIND_DEFAULT_OFFSET,
+            **adc_args,
+            ):  # pylint: disable=C0330
         """See class docstring for full details."""
-        self.scale = scale
-        self.offset = offset
-        self._adc = adafruit_ads1x15.analog_in.AnalogIn(
-            ivaldi.devices.adafruit.AdafruitADS1115(),
-            adafruit_ads1x15.ads1115.P0)
-
-    @property
-    def voltage(self):
-        """The voltage reported by the ADC, in volts."""
-        return self._adc.voltage
-
-    @property
-    def value_raw(self):
-        """The raw value reported by the ADC, as a 16-bit integer."""
-        return self._adc.value
-
-    @property
-    def value(self):
-        """The value of the direction, in degrees from north."""
-        return self._adc.value * self.scale + self.offset
+        super().__init__(scale=scale, offset=offset, **adc_args)
